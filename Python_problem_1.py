a492b6ee4a1caad7ad89358353c5ad050e27850c
diegofrosalesm <diego.250893@gmail.com>
2020-08-30 02:11:26 -0400

    Problem 1 solution

--- Python_problem_1.py
+++ Python_problem_1.py
@@ -2,6 +2,19 @@
 
 #Problem 1
 
-#If we list all the natural numbers below 10 that are multiples of 3 or 5,
-#we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+#Solución:
+    
+#Se inicia el resultado de la sumatoria en cero
+#Si i mod 3 y/o i mod 5 dan cero, i es múltiplo de alguno de esos números
+#por tanto i se suma al resultado final
+
+total = 0
+for i in range(1000):
+    if (i%3==0)or(i%5==0):
+        total += i
+
+print("El resultado es {}".format(total))
