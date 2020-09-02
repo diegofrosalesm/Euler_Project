9c4c914005ac8eb8fc0841b1ad978b5eea03d721
diegofrosalesm <diego.250893@gmail.com>
2020-09-01 22:00:35 -0400

    Problem 6 solution without explanation

--- Python_problem_6.py
+++ Python_problem_6.py
@@ -9,3 +9,7 @@
 # 
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 # =============================================================================
+n = 100
+dif = n*(n+1)*(3*n**2-n-2)//12
+
+print(dif)
