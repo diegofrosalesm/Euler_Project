075e829a5c4f03ad3aeefeca813b8faaa5760b8d
diegofrosalesm <diego.250893@gmail.com>
2020-09-02 00:52:40 -0400

    Problem 9 solution

--- Python_problem_9.py
+++ Python_problem_9.py
@@ -7,3 +7,30 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 # =============================================================================
+
+# =============================================================================
+# Solución:
+#     Ya que a y b deben ser números naturales, esto limita la búsqueda a cada par (a,b) tal que
+#     tanto a como b estén entre cero y la mitad del objetivo (target) (0, target/2)
+#     por cada número a en este rango, se prueban los valores posibles de b (desde 0 hasta n/2)
+#     hasta encontrar (a,b) enteros que hagan que a+b+(a**2 + b**2)**0.5 = target y a*b*c no sea 0
+# 
+# =============================================================================
+target = 1000
+
+a = target//2
+while a > 0:
+    b = target//2 - a
+    while b < target//2:
+        c = (a**2 + b**2)**0.5
+        total = a + b + c
+        prod = a*b*c
+        if total == target and prod != 0:
+            break
+        else:
+            b += 1
+    if total == target and prod != 0:
+        print("El resultado es: {}, a: {}, b: {}, c: {}".format(prod, a, b, c))
+        break
+    else:
+        a -= 1
